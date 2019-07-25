5da5c2e3392945efcc7ab23ebca4eb8ae41b3112
Virginie Beernaert <vbeernaert@yahoo.fr>
2019-07-25 22:27:51 +0200

    Deploy to heroku

---
+++ early-edition-back/public/precache-manifest.927746e05ce935dd121d901772e49bbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7deeaa501e3d98af993f213d2a1698c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d47c865a42a5ac012c15",
+    "url": "/static/css/main.d3b7dfe8.chunk.css"
+  },
+  {
+    "revision": "2f39c1b8a2512eebfc5e",
+    "url": "/static/js/2.895dd958.chunk.js"
+  },
+  {
+    "revision": "d47c865a42a5ac012c15",
+    "url": "/static/js/main.b7eb8730.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- early-edition-back/public/service-worker.js
+++ early-edition-back/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cf479e45a1c5a7c6718efd493bae931.js"
+  "/precache-manifest.927746e05ce935dd121d901772e49bbd.js"
 );
 
 self.addEventListener('message', (event) => {
